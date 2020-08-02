5b03b630df34a8b173cf4cf96876713dcdc46f27
Black-Rain06 <63678240+Black-Rain06@users.noreply.github.com>
2020-08-01 22:13:17 -0500

    Add files via upload
    Pauses, closes (stops), and resumes

---
+++ class_cam_detect.py
@@ -0,0 +1,91 @@
+import cv2
+import numpy as np
+import imutils
+import time
+import sys
+
+from imutils.video import FPS
+
+
+running = True
+
+class doorBellCam():
+
+    
+    def __init__(self):
+
+        self.paused = False
+        self.cap = cv2.VideoCapture(0)
+        
+        self.fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+        self.out = cv2.VideoWriter("output.avi", self.fourcc, 5.0, (480,480))
+        
+        ret, frame1 = self.cap.read()
+
+        while self.cap.isOpened() and not self.paused:
+            
+            _, frame2 = self.cap.read()
+            diff = cv2.absdiff(frame1, frame2)
+            gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(gray, (5,5), 0)
+            _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+            erode = cv2.erode(thresh.copy(), None, iterations=10)
+            dilated = cv2.dilate(erode, None, iterations=10)
+            contours, _ = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+            if len(contours) != 0:
+                c = max(contours, key = cv2.contourArea)
+                x,y,w,h = cv2.boundingRect(c)
+                if x > 50:
+                    cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
+                    print('Detected Motion')
+            else:
+                print('No motion or motion insignificant')
+
+            self.image = imutils.resize(frame1, width=850)
+            self.out.write(self.image)
+            cv2.imshow("feed", self.image)
+            frame1 = frame2
+
+            fps = FPS().start()
+            fps.update()
+
+            key = cv2.waitKey(20)
+
+            if key == ord('q'):
+                running = False
+                self.freeSpace()
+                sys.exit()
+                break
+
+            if key == ord('m'):
+                self.paused = True
+                cv2.imshow("feed", self.image)
+
+            
+    def resume(self):
+        self.paused = False
+
+
+    def freeSpace(self):
+
+        cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
+        self.cap.release()
+        self.out.release()
+
+
+def main():
+
+    cam = doorBellCam()
+    while cam.paused == True:
+        key2 = cv2.waitKey(20)
+        if key2 == ord('r'):
+            cam.resume()
+
+if __name__ == '__main__':
+    while True:
+        main()
+        if running == False:
+            print(running)
+            break

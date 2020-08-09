fb7acfe936bc2763ab70214ab350aba9b4a5f388
Black-Rain06 <63678240+Black-Rain06@users.noreply.github.com>
2020-08-09 18:58:36 -0500

    Add files via upload

--- kivytests.py
+++ kivytests.py
@@ -10,17 +10,15 @@ from kivy.uix.button import Button, Label
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
-from kivy.factory import Factory
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
 
 from datetime import datetime
 import kivy 
 import cv2
 
 
-notify = {}
-date = datetime.now()
+notify = {'No records':'',}
 
 class KivyCamera(Image):
     def __init__(self, capture, fps, **kwargs):
@@ -48,11 +46,8 @@ class KivyCamera(Image):
             cv2.rectangle(frame2,(x,y),(x+w*2,y+h*2),(0,0,255),2)
             cv2.rectangle(frame2,(x,y),(x+w*2,y+h*2),(0,255,0),2)
             self.notify.append([x,y,w,h])
+            date = datetime.now()
             notify[str(date)] = str([x,y,w,h])
-            #notify.append([x,y,w,h])
-            
-            #print('Detected Motion')
-        
         frame1 = frame2
         fps.update()
             
@@ -63,26 +58,12 @@ class KivyCamera(Image):
         image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         self.texture = image_texture
 
-
-#### THIS DOES NOT WORK - MAYBE TRY GETTING KIVY INPUT TO HANDLE DISCISSION ####
-        key = cv2.waitKey(20)
-
-        if key == ord('q'):
-            running = False
-            self.freeSpace()
-            sys.exit()
-            print('q')
-
-        if key == ord('m'):
-            self.paused = True
-            cv2.imshow(selGrf.title, self.image)
-            if key == ord('q'):
-                running = False
-                self.freeSpace()
-                sys.exit()
-
     def update_Notify(self):
         return self.notify
+
+    def freeSpace(self):
+        cv2.destroyAllWindows()
+        self.capture.release()
 
 class MainWindow(Screen):
     
@@ -90,89 +71,128 @@ class MainWindow(Screen):
             
                 super(MainWindow, self).__init__(**kwargs)
 
-                self.cols = GridLayout()
-                self.cols = 1
-                
-                self.cam = KivyCamera(cv2.VideoCapture(0), fps=120)
-                #self.noti = self.cam.update_Notify()
-                
-                self.inside = GridLayout()
-                self.inside.cols = 2
+                self.cols = GridLayout(cols = 1)
 
-                self.notify = Button(text='Notification', width=10)
-                self.notify.bind(on_release=self.notify_pressed)
-                self.inside.add_widget(self.notify)
+                self.cam = cv2.VideoCapture(0)
+                self.cam = KivyCamera(capture=self.cam, fps=120)
+                self.add_widget(self.cam)
+                self.noti = self.cam.update_Notify()
 
-                self.menu = Button(text='Menu', width=10)
-                self.menu.bind(on_press=self.menu_pressed)
-                self.inside.add_widget(self.menu)
-
-
-                self.add_widget(self.inside)
-                
-                
-##                key = cv2.waitKey(20)
-##                if key == ord('q'):
-##                    running = False
-##                    self.freeSpace()
-##                    sys.exit()
-##                    print('q')
-##
-##                if key == ord('m'):
-##                    self.paused = True
-##                    cv2.imshow(self.title, self.image)
-##                    if key == ord('q'):
-##                        running = False
-##                        self.freeSpace()
-##                        sys.exit()
-                
-
-        def freeSpace(self):
-            
-            cv2.destroyAllWindows()
-            self.capture.release()
-
-        def notify_pressed(self, instance):
-            print('hi')
-
-        def menu_pressed(self, instance):
-            print('menu pressed')
+        def menu_Switch(self, instance):
+            self.parent.current = 'third'  
     
 
-class SecondWindow(Screen):
+class NotificationWindow(Screen):
 
     def __init__(self, **kwargs):
-        super(SecondWindow, self).__init__(**kwargs)
+        super(NotificationWindow, self).__init__(**kwargs)
         
         self.inside = GridLayout()
-        self.inside.cols = 2
+        self.inside.cols = 1
 
-        self.notify = Button(text='Notification', width=10)
+        self.notify = Button(id='how', text='Notification', height="50dp", size_hint_y= None)
         self.notify.bind(on_release=self.notify_pressed)
-        self.inside.add_widget(self.notify)
-
-        self.menu = Button(text='Menu', width=10)
-        self.menu.bind(on_press=self.menu_pressed)
-        self.inside.add_widget(self.menu)
-
+        self.inside.add_widget(self.notify) 
+        
         self.label = Label(text='No records')
         self.inside.add_widget(self.label)
+        self.label_one = Label(text='No records')
+        self.inside.add_widget(self.label_one)
+        self.label_two = Label(text='No records')
+        self.inside.add_widget(self.label_two)
+        self.label_three = Label(text='No records')
+        self.inside.add_widget(self.label_three)
+        self.label_four = Label(text='No records')
+        self.inside.add_widget(self.label_four)
+        
+        self.label_five = Label(text='No records')
+        self.inside.add_widget(self.label_five)
+        self.label_six = Label(text='No records')
+        self.inside.add_widget(self.label_six)
+        self.label_seven = Label(text='No records')
+        self.inside.add_widget(self.label_seven)
+        self.label_eight = Label(text='No records')
+        self.inside.add_widget(self.label_eight)
+        self.label_nine = Label(text='No records')
+        self.inside.add_widget(self.label_nine)
+        
+        self.label_ten = Label(text='No records')
+        self.inside.add_widget(self.label_ten)
+        self.label_eleven = Label(text='No records')
+        self.inside.add_widget(self.label_eleven)
+        self.label_twelve = Label(text='No records')
+        self.inside.add_widget(self.label_twelve)
+        self.label_thirteen = Label(text='No records')
+        self.inside.add_widget(self.label_thirteen)
+        self.label_fifteen = Label(text='No records')
+        self.inside.add_widget(self.label_fifteen)
+        
+        self.label_sixteen = Label(text='No records')
+        self.inside.add_widget(self.label_sixteen)
+        self.label_seventeen = Label(text='No records')
+        self.inside.add_widget(self.label_seventeen)
+        self.label_eighteen = Label(text='No records')
+        self.inside.add_widget(self.label_eighteen)
+        self.label_nineteen = Label(text='No records')
+        self.inside.add_widget(self.label_nineteen)
+        
+        self.go_back = Button(text='Go Back')
+        self.go_back.bind(on_release=self.switch)
+        self.inside.add_widget(self.go_back)
+
+        self.go_back = Button(text='Menu')
+        self.go_back.bind(on_release=self.menu_Switch)
+        self.inside.add_widget(self.go_back)
 
         self.add_widget(self.inside)
 
 
     def notify_pressed(self, instance):
-        for element in notify:
-            self.label.text = element + ': ' + notify[element]
+        key = list(notify.keys())
+        value = list(notify.values())
+        count = len(notify)
+        
+        self.label.text = str(key[-1]) + ': '+ str(value[-1])
+        self.label_one.text = str(key[count-1]) + ': '+ str(value[count-1])
+        self.label_two.text = str(key[count-2]) + ': '+ str(value[count-2])
+        self.label_three.text = str(key[count-3]) + ': '+ str(value[count-3])
+        self.label_four.text = str(key[count-4]) + ': '+ str(value[count-4])
+        
+        self.label_five.text = str(key[count-5]) + ': '+ str(value[count-5])
+        self.label_six.text = str(key[count-6]) + ': '+ str(value[count-6])
+        self.label_seven.text = str(key[count-7]) + ': '+ str(value[count-7])
+        self.label_eight.text = str(key[count-8]) + ': '+ str(value[count-8])
+        self.label_nine.text = str(key[count-9]) + ': '+ str(value[count-9])
+        
+        self.label_ten.text = str(key[count-10]) + ': '+ str(value[count-10])
+        self.label_eleven.text = str(key[count-11]) + ': '+ str(value[count-11])
+        self.label_twelve.text = str(key[count-12]) + ': '+ str(value[count-12])
+        self.label_thirteen.text = str(key[count-13]) + ': '+ str(value[count-13])
+        self.label_fifteen.text = str(key[count-14]) + ': '+ str(value[count-14])
+        
+        self.label_sixteen.text = str(key[count-16]) + ': '+ str(value[count-16])
+        self.label_seventeen.text = str(key[count-17]) + ': '+ str(value[count-17])
+        self.label_eighteen.text = str(key[count-18]) + ': '+ str(value[count-18])
+        self.label_nineteen.text = str(key[count-19]) + ': '+ str(value[count-19])
+        
+    def switch(self, instance):
+        self.parent.current = 'main'
 
+    def menu_Switch(self, instance):
+        self.parent.current = 'third'
+    
     def menu_pressed(self, instance):
         print('menu pressed')
+
+class MenuWindow(Screen):
+    pass
+##    def __init__(self, **kwargs):
+##            super(NotificationWindow, self).__init__(**kwargs)
 
 
 class WindowManager(ScreenManager):
     pass
 
-#kv = Builder.load_file("my.kv")
 class CamApp(App):
     
     def build(self):

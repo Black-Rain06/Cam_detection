c1f7781cc0d4f009c92b5e4f3ddb0619a9c67006
Black-Rain06 <63678240+Black-Rain06@users.noreply.github.com>
2020-08-18 16:32:05 -0400

    Add files via upload

--- doorBellApp.py
+++ doorBellApp.py
@@ -2,27 +2,18 @@
 import cv2
 import sys
 import os
-from imutils.video import FPS
 
+
+from imutils.video import FPS
+import bluetooth
 
 class doorBellApp():
     def __init__(self):
+
         
         self.inname = ''
         self.exname = []
-        ###########
-        devs = os.listdir('/dev')
-        self.v = [int(dev[-1]) for dev in devs if dev.startswith('video')]
-        self.v = sorted(self.v)
-        #print(self.v)
-        '''                   '''
-        devs = os.listdir('/dev')
-        self.v = []
-        for dev in devs:
-            if dev.startswith('video'):
-                self.v.append(int(dev[-1]))
-            self.exname.append(dev)
-        ############
+        ###########        
         self.d = 0
         self.ava_dev = []
         searching = True
@@ -36,7 +27,6 @@ class doorBellApp():
                 if self.d == 0:
                     self.inname = sys.platform.capitalize()
                     searching = False
-                self.exname = []
             self.d += 1
 
         self.cam.read()
@@ -50,16 +40,8 @@ class doorBellApp():
     def available_dev(self):
         return self.ava_dev
 
-    def name(self):
-        return self.v#['h', 'd']
-
-    def set(self):
-        return cv2.VideoCapture(0)
-
     def destroy(self):
         cv2.destroyAllWindows()
 
     def release(self):
         self.cam.release()
-
-
--- kivytests.py
+++ kivytests.py
@@ -7,7 +7,8 @@ from imutils.video import FPS
 
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button, Label
+from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -21,6 +22,8 @@ from doorBellApp import doorBellApp
 from datetime import datetime
 import kivy 
 import cv2
+import numpy as np
+import bluetooth
 
 
 notify = {'No records':'',}
@@ -30,11 +33,10 @@ class KivyCamera(Image):
         super(KivyCamera, self).__init__(**kwargs)
         self.capture = capture
         Clock.schedule_interval(self.update, 1.0 / fps)
-        self.notify = []
+        #self.notify
 
     def update(self, dt):
         fps = FPS().start()
-
         _, self.frame1 = self.capture.read()
         _, self.frame2 = self.capture.read()
         diff = cv2.absdiff(self.frame1, self.frame2)
@@ -43,13 +45,12 @@ class KivyCamera(Image):
         _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
         erode = cv2.erode(thresh.copy(), None, iterations=10)
         dilated = cv2.dilate(erode, None, iterations=10)
-        contours, _ = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+        contours, hier = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         if len(contours) != 0:
             c = max(contours, key = cv2.contourArea)
             x,y,w,h = cv2.boundingRect(c)
-            cv2.rectangle(self.frame2,(x,y),(x+w*2,y+h*2),(0,0,255),4)
-            self.notify.append([x,y,w,h])
+            #self.notify.append([x,y,w,h])
+            rect = cv2.rectangle(self.frame2,(x,y),(x+w*2,y+h*2),(0,0,255),4)
             notify[str(datetime.now())] = str([x,y,w,h])
         self.frame1 = self.frame2
         fps.update()
@@ -60,9 +61,7 @@ class KivyCamera(Image):
             size=(self.frame1.shape[1], self.frame1.shape[0]), colorfmt='bgr')
         image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         self.texture = image_texture
-
-    def update_Notify(self):
-        return self.notify
+    
 
     def destroy(self):
         cv2.destroyAllWindows()
@@ -82,12 +81,11 @@ class Devices(Screen):
         layout = BoxLayout(orientation="vertical")
         self.devs = Label(text="Detected Devices: " +str(self.num))
         layout.add_widget(self.devs)
-        if len(self.capture.inname):
+        if len(self.capture.inname)>0:
             self.opt = Button(text=self.capture.inname)
             self.opt.bind(on_release=self.selectDev)
             layout.add_widget(self.opt)
-        
-        elif len(self.capture.inname) == 0 and len(self.capture.exname) == 0:
+        elif len(self.capture.inname) == 0:#and len(self.capture.exname) == 0:
             for i in self.capture.exname:
                 self.opt = Button(text=i)
                 self.opt.bind(on_release=self.selectDev)
@@ -131,7 +129,7 @@ class MainWindow(Screen):
         
         super(MainWindow, self).__init__(**kwargs)
 
-        self.cols = GridLayout(cols = 1)
+        self.cols = BoxLayout(orientation="horizontal", spacing=-800)
 
         self.capture = Devices()
         
@@ -139,32 +137,24 @@ class MainWindow(Screen):
         self.add_widget(self.cam)
         self.capture.destroy()
         #self.capture.release()
-        self.noti = self.cam.update_Notify()
 
     def menu_Switch(self, instance):
         self.parent.current = 'third'
     
-##class m(Screen, Image):
-##
-##    def __init__(self, capture, **kwargs):
-##        
-##        super(m, self).__init__(**kwargs)
-##
-##        self.add_widget(MainWindow(cv2.VideoCapture(0)))
-##        pass
         
 class NotificationWindow(Screen):
 
     def __init__(self, **kwargs):
         super(NotificationWindow, self).__init__(**kwargs)
         
-        self.inside = GridLayout()
-        self.inside.cols = 1
-
-        self.notify = Button(id='how', text='Notification', height="50dp", size_hint_y= None)
-        self.notify.bind(on_release=self.notify_pressed)
-        self.inside.add_widget(self.notify) 
+##        self.inside = GridLayout()
+##        self.inside.cols = 1
+##
+##        self.notify = Button(id='how', text='Notification', height="50dp", size_hint_y= None)
+##        self.notify.bind(on_release=self.notify_pressed)
+##        self.inside.add_widget(self.notify)
         
+        '''
         self.label = Label(text='No records')
         self.inside.add_widget(self.label)
         self.label_one = Label(text='No records')
@@ -214,8 +204,37 @@ class NotificationWindow(Screen):
         self.go_back = Button(text='Menu')
         self.go_back.bind(on_release=self.menu_Switch)
         self.inside.add_widget(self.go_back)
+        '''
 
-        self.add_widget(self.inside)
+        self.layout = BoxLayout(orientation="vertical", spacing=-200)
+
+        self.label = Label(text='No records')
+        self.layout.add_widget(self.label)
+        self.label_one = Label(text='No records')
+        self.layout.add_widget(self.label_one)
+        self.label_two = Label(text='No records')
+        self.layout.add_widget(self.label_two)
+        self.label_three = Label(text='No records')
+        self.layout.add_widget(self.label_three)
+        self.label_four = Label(text='No records')
+        self.layout.add_widget(self.label_four)
+        
+        self.layout2 = BoxLayout(orientation="horizontal")
+        
+        self.notify = Button(text='Notification', height="50dp", size_hint_y= None)
+        self.notify.bind(on_release=self.notify_pressed)
+        self.layout2.add_widget(self.notify)
+
+        self.go_back = Button(text='Go Back', height="50dp", size_hint_y= None)
+        self.go_back.bind(on_release=self.switch)
+        self.layout2.add_widget(self.go_back)
+
+        self.go_back = Button(text='Menu', height="50dp", size_hint_y= None)
+        self.go_back.bind(on_release=self.menu_Switch)
+        self.layout2.add_widget(self.go_back)
+        
+        self.add_widget(self.layout)
+        self.add_widget(self.layout2)
 
 
     def notify_pressed(self, instance):
@@ -228,7 +247,7 @@ class NotificationWindow(Screen):
         self.label_two.text = str(key[count-2]) + ': '+ str(value[count-2])
         self.label_three.text = str(key[count-3]) + ': '+ str(value[count-3])
         self.label_four.text = str(key[count-4]) + ': '+ str(value[count-4])
-        
+        '''
         self.label_five.text = str(key[count-5]) + ': '+ str(value[count-5])
         self.label_six.text = str(key[count-6]) + ': '+ str(value[count-6])
         self.label_seven.text = str(key[count-7]) + ': '+ str(value[count-7])
@@ -241,11 +260,11 @@ class NotificationWindow(Screen):
         self.label_thirteen.text = str(key[count-13]) + ': '+ str(value[count-13])
         self.label_fifteen.text = str(key[count-14]) + ': '+ str(value[count-14])
         
-        self.label_sixteen.text = str(key[count-16]) + ': '+ str(vue[count-16])
+        self.label_sixteen.text = str(key[count-16]) + ': '+ str(value[count-16])
         self.label_seventeen.text = str(key[count-17]) + ': '+ str(value[count-17])
         self.label_eighteen.text = str(key[count-18]) + ': '+ str(value[count-18])
         self.label_nineteen.text = str(key[count-19]) + ': '+ str(value[count-19])
-        
+        '''
     def switch(self, instance):
         self.parent.current = 'main'
 

62afa5c623e2ab3fd9d8d6c6c49170caedb9534f
Black-Rain06 <63678240+Black-Rain06@users.noreply.github.com>
2020-09-27 14:27:14 -0500

    Add files via upload

--- webcamvideostream.py
+++ webcamvideostream.py
@@ -8,6 +8,7 @@ class WebcamVideoStream:
         print("init")
         self.stream = cv2.VideoCapture(src)
         (self.grabbed, self.frame) = self.stream.read()
+        (self.grabbed1, self.frame1) = self.stream.read()
         self.stopped = False
         time.sleep(2.0)
     
@@ -21,25 +22,24 @@ class WebcamVideoStream:
     def update(self):
         print("read")
         while True:
-            if self.stopped:
-                return
-            
             (self.grabbed, self.frame) = self.stream.read()
-            
-            gray = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
+            (self.grabbed1, self.frame1) = self.stream.read()
+            diff = cv2.absdiff(self.frame, self.frame1)
+            gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
             blur = cv2.GaussianBlur(gray, (5,5), 0)
-
-            ret,thresh = cv2.threshold(blur,20,255,cv2.THRESH_BINARY)
-            erode = cv2.erode(thresh, None, iterations=10)
-            dilated = cv2.dilate(erode, None, iterations=10)
-
-            contours, hierarchy = cv2.findContours(dilated.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+            _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+            erode = cv2.erode(thresh.copy(), None, iterations=5)
+            dilated = cv2.dilate(erode, None, iterations=5)
+            contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             
             for contour in contours:
-                if cv2.contourArea(contour) < 10000:
-                    (x,y,w,h) = cv2.boundingRect(contour)
-                    cv2.rectangle(self.frame, (x,y), (x+w, y+h), (0,255,0), 3)
-    
+                c = max(contours, key = cv2.contourArea)
+                (x, y, w, h) = cv2.boundingRect(c)
+
+
+                cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                cv2.putText(self.frame, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                            1, (0, 0, 255), 3)                                                                    
     def read(self):
         return self.frame
     

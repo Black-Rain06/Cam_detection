104e85a2f70589c353d0290c70b5c49d48a3a632
Black-Rain06 <63678240+Black-Rain06@users.noreply.github.com>
2020-09-27 14:04:07 -0500

    Add files via upload

--- webcamvideostream.py
+++ webcamvideostream.py
@@ -25,6 +25,20 @@ class WebcamVideoStream:
                 return
             
             (self.grabbed, self.frame) = self.stream.read()
+            
+            gray = cv2.cvtColor(self.frame,cv2.COLOR_BGR2GRAY)
+            blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+            ret,thresh = cv2.threshold(blur,20,255,cv2.THRESH_BINARY)
+            erode = cv2.erode(thresh, None, iterations=10)
+            dilated = cv2.dilate(erode, None, iterations=10)
+
+            contours, hierarchy = cv2.findContours(dilated.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+            
+            for contour in contours:
+                if cv2.contourArea(contour) < 10000:
+                    (x,y,w,h) = cv2.boundingRect(contour)
+                    cv2.rectangle(self.frame, (x,y), (x+w, y+h), (0,255,0), 3)
     
     def read(self):
         return self.frame
